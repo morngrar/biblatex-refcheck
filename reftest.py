43ed5d6119d47fad32b4e1887b60f5df3b2ebbf5
Svein-Kåre Bjørnsen <morngrar@users.noreply.github.com>
2017-05-23 13:40:18 +0200

    Added script to repo

---
+++ reftest.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""Script for testing BibLaTeX bibliography file for comma-errors.
+
+Takes bibliography-file as input, reports on stdout if the file looks OK or not, and also provides function check_file() which returns True or False respectively, for use in automation of LaTeX compilation.
+"""
+
+import sys
+
+def check_file():
+    i = 0
+    fault = False
+    with open(sys.argv[1]) as f:
+        for line in f:
+            i += 1
+            if len(line) > 2:
+                if line[-2] is not ",":
+                    print("COMMA-ERROR on LINE {0}!".format(str(i)))
+                    fault = True
+
+        if not fault:
+            print("Everything checks out: OK")
+            return True
+        else:
+            print("Errors need to be fixed before continuing with LaTeX compilation!")
+            return False
+
+if __name__=="__main__":
+    check_file()
