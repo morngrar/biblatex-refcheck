f86219aeebd8dbc9b99dbaa5769ce79b7e138c1d
Svein-Kåre Bjørnsen <morngrar@users.noreply.github.com>
2017-09-13 22:45:28 +0200

    Fixed docstring

--- reftest.py
+++ reftest.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
-"""Script for testing BibLaTeX bibliography file for comma-errors.
+"""
+Script for testing BibLaTeX bibliography file for comma-errors.
 
-Takes bibliography-file as input, reports on stdout if the file looks OK or not, and also provides function check_file() which returns True or False respectively, for use in automation of LaTeX compilation.
+Takes bibliography-file as input, reports on stdout if the file looks OK or not, 
+and also provides function check_file() which returns True or False respectively, 
+for use in automation of LaTeX compilation.
 """
 
 def check_file(filename):
